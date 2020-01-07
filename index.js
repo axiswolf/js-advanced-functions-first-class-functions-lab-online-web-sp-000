4e8419c356c1fcd505d980f983e4a9897bee850f
Karen V <karenvo@usc.edu>
2020-01-07 11:00:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ const fareDoubler = createFareMultiplier(2)
 const fareTripler = createFareMultiplier(3)
 
 function selectDifferentDrivers(drivers, f) {
-  return drivers(f)
+  return f(drivers)
 }
