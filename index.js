4c77e782652f5c6ddb79ad4badda7e04a7896bc3
Karen V <karenvo@usc.edu>
2020-01-07 10:33:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const returnFirstTwoDrivers = function(drivers) {
   return [drivers[0], drivers[1]]
 }
 
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)
+}
 function selectingDrivers() {
 
 }
